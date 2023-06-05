092d8044d8d6c3c66cbc4646a055cd81fdf7a19a
tine <tine@gmail.com>
2023-06-05 08:32:02 +0200

    add clangd

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "cmake", "pyright", "rust_analyzer", "marksman", "hls" },
+      ensure_installed = { "lua_ls", "cmake", "pyright", "rust_analyzer", "marksman", "clangd" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
